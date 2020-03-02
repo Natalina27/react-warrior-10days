cdcb632fcec31e1a7e5ee1469975715b60fed0d1
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-02 17:00:32 +0100

    day1 - components tree

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MovieItem(props) {
-    console.log('props= ', props);
+const movie = {
+    title: "Avengers: Infinity War",
+    vote_average: 8.5,
+    image: "http://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    overview:
+    "As the Avengers and their allies have continued to protect the world from threats"
+};
+
+    function Image(props) {
+        console.log('Image props = ', props);
+
+        return(
+            <img src={props.src} alt={props.alt}/>
+        );
+    }
+
+    function MovieItem(props) {
+    console.log('MovieItem props= ', props);
+    const { data: { title, vote_average, image}} = props; //destruction obj
 
     return(
         <div>
-            <p>{props.title}</p>
-            <p>{props.vote_average}</p>
+            <Image src={image} alt={title}/>
+            <p>{title}</p>
+            <p>{vote_average}</p>
         </div>
     );
 }
@@ -15,7 +33,7 @@ function MovieItem(props) {
 function App(){
     return(
         <div>
-            <MovieItem title='Title new' vote_average={10.1}/>
+            <MovieItem data={movie}/>
         </div>
     );
 }
