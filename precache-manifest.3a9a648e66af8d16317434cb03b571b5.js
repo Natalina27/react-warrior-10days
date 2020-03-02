499df464e36391f98d0ac0e4a84f5b2c97d356ad
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-02 16:10:58 +0100

    Updates

---
+++ precache-manifest.3a9a648e66af8d16317434cb03b571b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335037e9dfe379bb20565eeed7f95963",
+    "url": "/react-warrior-10days/index.html"
+  },
+  {
+    "revision": "9c9edd5d40a46726cef8",
+    "url": "/react-warrior-10days/static/js/2.c9e6fee4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-warrior-10days/static/js/2.c9e6fee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe6870b24b59b5a6700",
+    "url": "/react-warrior-10days/static/js/main.6d9df96e.chunk.js"
+  },
+  {
+    "revision": "e97d6bff6ce6da870489",
+    "url": "/react-warrior-10days/static/js/runtime-main.b1ade679.js"
+  }
+]);
