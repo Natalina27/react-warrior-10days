fa1519c0770e64be94facd9abf971da6cd7c2862
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-02 16:09:35 +0100

    gh-pages added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function MovieItem(props) {
+    console.log('props= ', props);
+
+    return(
+        <div>
+            <p>{props.title}</p>
+            <p>{props.vote_average}</p>
+        </div>
+    );
+}
+
+function App(){
+    return(
+        <div>
+            <MovieItem title='Title new' vote_average={10.1}/>
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
