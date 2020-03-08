02bdb248e52cbd76e2e61146a4bc0098665cfb5f
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-08 12:10:10 +0100

    day 3 cra

---
+++ src/Components/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import './App.module.css';
+// import classnames from 'classnames';
+
+function App() {
+  return <div>Hello ReactWarriors</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,80 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './Components/App/App.js'
 
 
 
-    const movie = {
-    title: "Avengers: Infinity War",
-    vote_average: 8.5,
-    image: "http://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-    overview:
-    "As the Avengers and their allies have continued to protect the world from threats"
-};
-
-    function Image(props) {
-        console.log('Image props = ', props);
-
-        return(
-            <img src={props.src} alt={props.alt} className="image"/>
-        );
-    }
 
 
-    class MovieItem extends React.Component{ //MovieItem = new React.Component()
-        constructor(){
-            super();
-            this.state = {
-                show: false,
-                like: false
-            };
-        }
+//     const movie = {
+//     title: "Avengers: Infinity War",
+//     vote_average: 8.5,
+//     image: "http://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+//     overview:
+//     "As the Avengers and their allies have continued to protect the world from threats"
+// };
+//
+//     function Image(props) {
+//         console.log('Image props = ', props);
+//
+//         return(
+//             <img src={props.src} alt={props.alt} className="image"/>
+//         );
+//     }
+//
+//
+//     class MovieItem extends React.Component{ //MovieItem = new React.Component()
+//         constructor(){
+//             super();
+//             this.state = {
+//                 show: false,
+//                 like: false
+//             };
+//         }
+//
+//         toggleOverview = ()=> {
+//             console.log('show');
+//             this.setState({
+//                 show: !this.state.show
+//             });
+//         };
+//
+//         handleLike = () => {
+//             this.setState({
+//                 like: !this.state.like
+//
+//             });
+//         };
+//
+//         render(){
+//             const { data: { title, vote_average, image, overview}} = this.props; //destruction obj
+//             console.log(' state =  ', this.state);
+//             console.log(this);
+//
+//             return(
+//                 <div>
+//                     <Image src={image} alt={title} />
+//                     <p>{title}</p>
+//                     <p>{vote_average}</p>
+//                     <div>
+//                         <button type="button"
+//                                 onClick={this.toggleOverview}
+//                         >{this.state.show ? 'HIDE' : 'SHOW'}</button>
+//                         <button
+//                             type="button"
+//                             onClick={this.handleLike}
+//                             style={{background: this.state.like ? 'blue':'white'}}
+//                         >LIKE</button>
+//                     </div>
+//                     { this.state.show ? <p>{overview}</p> : null}
+//                 </div>
+//             );
+//         }
+// }
 
-        toggleOverview = ()=> {
-            console.log('show');
-            this.setState({
-                show: !this.state.show
-            });
-        };
-
-        handleLike = () => {
-            this.setState({
-                like: !this.state.like
-
-            });
-        };
-
-        render(){
-            const { data: { title, vote_average, image, overview}} = this.props; //destruction obj
-            console.log(' state =  ', this.state);
-            console.log(this);
-
-            return(
-                <div>
-                    <Image src={image} alt={title} />
-                    <p>{title}</p>
-                    <p>{vote_average}</p>
-                    <div>
-                        <button type="button"
-                                onClick={this.toggleOverview}
-                        >{this.state.show ? 'HIDE' : 'SHOW'}</button>
-                        <button
-                            type="button"
-                            onClick={this.handleLike}
-                            style={{background: this.state.like ? 'blue':'white'}}
-                        >LIKE</button>
-                    </div>
-                    { this.state.show ? <p>{overview}</p> : null}
-                </div>
-            );
-        }
-}
-
-        function App(){ //parent of MovieItem
-    return(
-        <div>
-            <MovieItem data={movie}/>
-        </div>
-    );
-}
+    //     function App(){ //parent of MovieItem
+    // return(
+    //     <div>
+    //         {/*<MovieItem data={movie}/>*/}
+    //         'Hello world'
+    //     </div>
+    // );
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
