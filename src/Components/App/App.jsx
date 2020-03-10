f324f20cdf632a81750cab10c1876b60d86957b0
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-10 15:28:52 +0100

    day 3 cra added Class App

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { moviesData } from '/Users/natalyamyunster/Desktop/REACT_REDUX/react-warrior-10days/src/moviesData.js';
 
-
-
-// import './App.module.css';
-// import classnames from 'classnames';
-
 console.log(moviesData);
 
-function App() {
-  return <div>{moviesData[0].title}</div>;
+class App extends React.Component{
+  render() {
+    console.log(this);
+    return <div>{moviesData[0].title}</div>;
+  }
 }
+
 
 export default App;
