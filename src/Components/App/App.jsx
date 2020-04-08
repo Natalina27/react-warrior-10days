414c97b6e78e24edf40c7f237860e245396403c5
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-08 14:14:03 +0200

    day 3 cra added map method for movies list rendering

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,19 +3,18 @@ import {moviesData} from "../../moviesData";
 
 console.log(moviesData);
 
-class App extends React.Component{
+class App extends React.Component {
 
-  state = {
-    movies: moviesData,
-  };
-  render() {
-    console.log(this);
-    return(
-        <div>
-          {this.state.movies.map((movie) => <p>{movie.title}</p>)}
-            </div>
+    state = {
+        movies: moviesData,
+    };
+
+    render() {
+        console.log(this);
+        return (
+            <div>{this.state.movies.map((movie) => <p>{movie.title}</p>)}</div>
         )
-  }
+    }
 }
 
 
